ef1bd58c112f1723f54827deab2f08ce4bbb73fa
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-03-11 09:39:27 +0300

    updated index.js to include cde as specified

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = 'Sam';
+    employee["streetAddress"] = '12 Broadway';
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = { ...employee };
+    delete newEmployee[name];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name];
+    return employee;
+}
